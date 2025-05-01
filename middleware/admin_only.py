6706da599789ddb01647cd50ac257515b9253d2b
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2025-05-01 14:55:29 +0700

    Ubah sistem admin

--- middleware/admin_only.py
+++ middleware/admin_only.py
@@ -1,12 +1,13 @@
 from flask import jsonify
-from flask_jwt_extended import get_jwt
+from flask_jwt_extended import get_jwt_identity
+from models import User
 
 def admin_only():
     def decorator(fn):
         def wrapper(*args, **kwargs):
-            claims = get_jwt()
-            role = claims["role"]
-            if role != 'admin':
+            current_user = get_jwt_identity()
+            user = User.objects(id=current_user, role='admin').first()
+            if not user:
                 return jsonify(
                     {
                         'code': 403,
