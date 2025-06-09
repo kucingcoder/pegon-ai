e03517c6c024be4d9c5449920543248b8390508d
M. Ibrahim Hanif <137396727+kucingcoder@users.noreply.github.com>
2025-06-09 16:08:44 +0700

    sedikit perbaikan

--- controllers/payment.py
+++ controllers/payment.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, request
 
 payment_bp = Blueprint('payment', __name__)
-@payment_bp.route('/notification', methods=['GET'])
+@payment_bp.route('/notification', methods=['POST'])
 def notif():
     data = request.get_json()
     print(data)
     return 200
 
-@payment_bp.route('/error', methods=['GET'])
+@payment_bp.route('/error', methods=['POST'])
 def error():
     data = request.get_json()
     print(data)
@@ -15,6 +15,4 @@ def error():
 
 @payment_bp.route('/finish', methods=['GET'])
 def finish():
-    data = request.get_json()
-    print(data)
     return 'Hello world'
