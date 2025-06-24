ddb9b76bc2949269d77b4673dccecad870872ee2
M. Ibrahim Hanif <137396727+kucingcoder@users.noreply.github.com>
2025-06-24 19:12:39 +0700

    Menambahkan informasi tanggal kedaluwarsa pada respons profil pengguna

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -254,7 +254,8 @@ def profile():
             'name': user.name,
             'sex': user.sex,
             'date_of_birth': user.date_of_birth.strftime("%Y-%m-%d") if user.date_of_birth else None,
-            'category': user.category
+            'category': user.category,
+            'expired': 'has no expiration date' if user.category == 'free' else user.expired_at.strftime("%d/%m/%Y")
         }
 
     return jsonify(
